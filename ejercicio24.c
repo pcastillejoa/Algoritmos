6d8a10b89a7e8c83e2537e752a9f46d227d0f455
pcastillejoa <37603634+pcastillejoa@users.noreply.github.com>
2018-03-24 17:07:56 -0300

    Update ejercicio24.c

--- ejercicio24.c
+++ ejercicio24.c
@@ -1,63 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define ERROR_MSJ "Error:"
+#define DIA_INVALIDO "Ingrese un dia entre 1-366" 
 
 int main()
 {
 
-    int dia=1;
+    int dia;
     
-    printf("Ingrese numero del dia");
+    printf("Ingrese numero del dia del año\n");
     scanf("%d", &dia);
     
+		
+    if ((dia-1)%7==0)
+    printf ("El dia es domingo\n");
     
-    switch(dia)
-    {
-        case 1:
-        dia = + 7;
-        printf("Domingo");
-            break;
-            
-        case 2: 
-        dia = + 7;
-        printf("Lunes");
-            break;
-            
-        case 3: 
-        dia = + 7;
-        printf("Martes");
-            break;
-            
-        case 4: 
-        dia = + 7;
-        printf("Miercoles");
-            break;
-            
-        case 5: 
-        dia = + 7;
-        printf("Jueves");
-            break;
-            
-        case 6: 
-        dia = + 7;
-        printf("Viernes");
-            break;
-            
-        case 7: 
-        dia = + 7;
-        printf("Sabado");
-            break;
-            
-        default: printf("Eror! I");
+    else if ((dia-2)%7==0)
+    printf ("El dia es lunes\n");
+    
+    else if ((dia-3)%7==0)
+    printf ("El dia es martes\n");
+    
+    else if ((dia-4)%7==0)
+    printf ("El dia es miercoles\n");
+    
+    else if ((dia-5)%7==0)
+    printf ("El dia es jueves\n");
+    
+    else if ((dia-6)%7==0)
+    printf ("El dia es viernes\n");
+    
+    else 
+    printf ("El dia es sabado\n");
+
+if (dia<=1||dia>=366)
+	fprintf(stderr, "%s : %s\n" , ERROR_MSJ , DIA_INVALIDO);
+	return EXIT_FAILURE;
 	
-    }
-    return 0;
-   
 }
-	
-	
-	
-	
-	
-	
+    
 	
 	
 	

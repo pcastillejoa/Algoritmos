c7eb7b6e6733dbc8e6d0c3a2c60e9dd1702f743b
pcastillejoa <37603634+pcastillejoa@users.noreply.github.com>
2018-03-24 14:42:20 -0300

    Add files via upload

---
+++ ejercicio24.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int dia=1;
+    
+    printf("Ingrese numero del dia");
+    scanf("%d", &dia);
+    
+    
+    switch(dia)
+    {
+        case 1:
+        dia = + 7;
+        printf("Domingo");
+            break;
+            
+        case 2: 
+        dia = + 7;
+        printf("Lunes");
+            break;
+            
+        case 3: 
+        dia = + 7;
+        printf("Martes");
+            break;
+            
+        case 4: 
+        dia = + 7;
+        printf("Miercoles");
+            break;
+            
+        case 5: 
+        dia = + 7;
+        printf("Jueves");
+            break;
+            
+        case 6: 
+        dia = + 7;
+        printf("Viernes");
+            break;
+            
+        case 7: 
+        dia = + 7;
+        printf("Sabado");
+            break;
+            
+        default: printf("Eror! I");
+	
+    }
+    return 0;
+   
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+

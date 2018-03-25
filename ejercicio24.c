fcb68e196a899c9603bea886cc4f79f7e1808f1f
pcastillejoa <37603634+pcastillejoa@users.noreply.github.com>
2018-03-25 12:07:30 -0300

    Update ejercicio24.c

--- ejercicio24.c
+++ ejercicio24.c
@@ -7,40 +7,54 @@
 int main()
 {
 
-    int dia;
-    
+    int dia; 
+    int modulo;
+  
     printf("Ingrese numero del dia del año\n");
-    scanf("%d", &dia);
-    
-		
-    if ((dia-1)%7==0)
-    printf ("El dia es domingo\n");
-    
-    else if ((dia-2)%7==0)
-    printf ("El dia es lunes\n");
-    
-    else if ((dia-3)%7==0)
-    printf ("El dia es martes\n");
-    
-    else if ((dia-4)%7==0)
-    printf ("El dia es miercoles\n");
-    
-    else if ((dia-5)%7==0)
-    printf ("El dia es jueves\n");
-    
-    else if ((dia-6)%7==0)
-    printf ("El dia es viernes\n");
-    
-    else 
-    printf ("El dia es sabado\n");
-
-if (dia<=1||dia>=366)
+    scanf("%d",&dia);
+          
+	 if (dia<1||dia>=366)
 	fprintf(stderr, "%s : %s\n" , ERROR_MSJ , DIA_INVALIDO);
-	return EXIT_FAILURE;
-	
-}
+	else if (dia>=1 || dia<366){
     
-	
+        modulo=(dia-1)%7;
+ 
+    switch (modulo)
+    {
+        case 0:
+        printf("Domingo\n");
+            break;
+            
+        case 1: 
+        printf("Lunes\n");
+            break;
+            
+        case 2: 
+        printf("Martes\n");
+            break;
+            
+        case 3: 
+        printf("Miercoles\n");
+            break;
+            
+        case 4: 
+        printf("Jueves\n");
+            break;
+            
+        case 5: 
+        printf("Viernes\n");
+            break;
+            
+        case 6: 
+        printf("Sabado\n");
+            break;
+                    
+        default:
+        printf("Eror! Ingrese un numero valido.\n");
+    }
+}
+return EXIT_FAILURE;
+}
 	
 	
 	

b3255873b085553af9ff64805dd7bfb3ee79ceab
pcastillejoa <37603634+pcastillejoa@users.noreply.github.com>
2018-04-19 23:40:06 -0300

    Add files via upload

---
+++ probabilidades_a.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int num_rand (void)
+{
+	int a;
+	int result;
+	
+    srand(time(NULL));
+    
+    a = (float)rand() / RAND_MAX;
+    
+    if (0 < a && a < 0.2)
+     result=0;
+     
+    else if (0.2 < a && a < 1)
+      result=1;
+ 
+    else 
+      result=0; 
+    
+    return a;
+    
+}     

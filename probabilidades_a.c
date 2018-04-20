6bc13b7ea78619cdac4d55aaa6ac4da649f8d44e
pcastillejoa <37603634+pcastillejoa@users.noreply.github.com>
2018-04-19 23:45:39 -0300

    Update probabilidades_a.c

--- probabilidades_a.c
+++ probabilidades_a.c
@@ -1,37 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "probabilidades_a.h"
 
-int probabilidad_a (void)
-{
-	
-	float a;
-	
-    srand(time(NULL));
-    
-    a = (float)rand() / RAND_MAX;
-    
-    return a;
-    
-}     
-
-    int main (void)
-{
-    float prob;
-    int res;
-    
-    prob = probabilidad_a ();
-    
-    if (0 < prob < 0.2)
-    res=0;
-     
-    else if (0.2 < prob < 1)
-    res=1;
  
-    else 
-    res=0;
+int main (void)
+{
+    int prob;
     
-    printf ("%d\n", res);
+    prob = num_rand ();
+ 
+    printf ("%d\n", prob);
 
     return 0;
 }    
+

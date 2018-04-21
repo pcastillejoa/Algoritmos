817801e77c5db3cfb21567ae2f28a76bc559f78a
pcastillejoa <37603634+pcastillejoa@users.noreply.github.com>
2018-04-21 17:28:12 -0300

    Update probabilidades_a.c

--- probabilidades_a.c
+++ probabilidades_a.c
@@ -6,11 +6,26 @@
  
     int main (void)
 {
-    int final;
+  
+	float proba;
+	
+	int result;
+	
+	
+    srand(time(NULL));
     
-    final = num_rand ();
- 
-    printf ("%d\n", final);
-
-    return 0;
+    
+    proba = (float)rand() / RAND_MAX;
+    
+    
+     if (0 < proba && proba < 0.2)
+       result=0;
+     
+     else if (0.2 < proba && proba < 1)
+       result=1;
+  
+     else 
+       result=0; 
+    
+    return result;
 }    

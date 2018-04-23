ef0de5c67d8ba43a7c5e89f2c98cc3aafe87f66f
pcastillejoa <37603634+pcastillejoa@users.noreply.github.com>
2018-04-22 21:32:10 -0300

    Update probabilidades_a.h

--- probabilidades_a.h
+++ probabilidades_a.h
@@ -1,29 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-int num_rand (void)
-{
-	float proba;
-	
-	int result;
-	
-	
-    srand(time(NULL));
-    
-    
-    proba = (float)rand() / RAND_MAX;
-    
-    
-     if (0 < proba && proba < 0.2)
-       result=0;
-     
-     else if (0.2 < proba && proba < 1)
-       result=1;
-  
-     else 
-       result=0; 
-    
-    return result;
-    
-}     
+
+int num_rand (void);

ba6d3152acfd0df8b3b78b44109d169fba99ffb6
pcastillejoa <37603634+pcastillejoa@users.noreply.github.com>
2018-04-22 22:16:59 -0300

    Add files via upload

---
+++ probabilidades_c.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "probabilidades_c.h"
+
+char num_randc (void)
+{
+	float proba;
+	
+	char result= '\0';
+	
+	
+    srand(time(NULL));
+    
+    
+    proba = (float)rand() / RAND_MAX;
+    
+    
+     if (0 < proba && proba < 0.5)
+       result= 'H';
+     
+     else if (0.5 < proba && proba < 1)
+       result= 'T';
+    
+    return result;
+    
+}    
+
+int main (void)
+{
+   char final;
+   
+   final = num_randc();
+   
+   printf ("%c\n", final);
+  
+   return 0;
+
+}
